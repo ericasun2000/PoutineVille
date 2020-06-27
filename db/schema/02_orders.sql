e8767f004f3be67321c0b59d15e520b90ab9b9c9
Adel Rashed <adel_rashed@outlook.com>
2020-06-27 18:40:56 +0000

    fixed sumbitted typo error to submitted

--- db/schema/02_orders.sql
+++ db/schema/02_orders.sql
@@ -3,6 +3,6 @@ DROP TABLE IF EXISTS orders CASCADE;
 CREATE TABLE orders (
   id SERIAL PRIMARY KEY NOT NULL,
   telephone VARCHAR(255) NOT NULL,
-  sumbitted_at TIMESTAMPZ DEFAULT NOW(),
-  completed_at TIMESTAMPZ
+  submitted_at TIMESTAMP DEFAULT NOW(),
+  completed_at TIMESTAMP
 );
