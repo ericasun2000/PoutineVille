19396a4fbc067784744c2ebfbae662ef8e41796a
Adel Rashed <adel_rashed@outlook.com>
2020-06-27 18:50:35 +0000

    created seeds for the ordered_dishes  entity

---
+++ db/seeds/03_ordered_dishes.sql
@@ -0,0 +1,12 @@
+INSERT INTO ordered_dishes (order_id,dish_id,quantity,price) VALUES(1,1,1,15);
+INSERT INTO ordered_dishes (order_id,dish_id,quantity,price) VALUES(1,2,1,15);
+
+INSERT INTO ordered_dishes (order_id,dish_id,quantity,price) VALUES(2,4,5,5);
+
+INSERT INTO ordered_dishes (order_id,dish_id,quantity,price) VALUES(3,1,1,15);
+
+INSERT INTO ordered_dishes (order_id,dish_id,quantity,price) VALUES(4,3,2,20);
+
+
+
+
