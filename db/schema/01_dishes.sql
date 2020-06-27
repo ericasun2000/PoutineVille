289e157a2859facfd10125a2a74f03d7863fab8c
Adel Rashed <adel_rashed@outlook.com>
2020-06-27 16:45:00 +0000

    created the schema for dishes entity

---
+++ db/schema/01_dishes.sql
@@ -0,0 +1,10 @@
+
+DROP TABLE IF EXISTS dishes CASCADE;
+
+CREATE TABLE dishes (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+  price INTEGER NOT NULL,
+  image_url VARCHAR(255) NOT NULL
+);
