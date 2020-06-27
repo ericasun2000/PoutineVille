83e5ae7b885834d0cfa647c6995b8f8958fe89f1
Adel Rashed <adel_rashed@outlook.com>
2020-06-27 23:29:19 +0000

    removed the users and widgets routes from server file

--- server.js
+++ server.js
@@ -36,14 +36,10 @@ app.use(express.static(__dirname + "/public"));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
-const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
 const dishesRoutes = require("./routes/dishes");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
-app.use("/api/users", usersRoutes(dbHelpers));
-app.use("/api/widgets", widgetsRoutes(dbHelpers));
 app.use("/api/dishes", dishesRoutes(dbHelpers));
 // Note: mount other resources here, using the same pattern above
 
