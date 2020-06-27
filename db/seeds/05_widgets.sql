48106ce8f36744b4c155655aef1ec5e44ace1024
Adel Rashed <adel_rashed@outlook.com>
2020-06-27 18:51:30 +0000

    removed widgets schema and seeds

--- db/seeds/05_widgets.sql
+++
@@ -1,4 +0,0 @@
--- Widgets table seeds here (Example)
-INSERT INTO widgets (name) VALUES ('Sprockets');
-INSERT INTO widgets (name) VALUES ('Chains');
-INSERT INTO widgets (name) VALUES ('Bearings');
