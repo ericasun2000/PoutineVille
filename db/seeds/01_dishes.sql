2086dbb3669e0ee6c0996a5ab8e824ee2d551e65
Adel Rashed <adel_rashed@outlook.com>
2020-06-27 17:48:07 -0400

    added new seeds to dishes entity

--- db/seeds/01_dishes.sql
+++ db/seeds/01_dishes.sql
@@ -1,4 +1,16 @@
-INSERT INTO dishes (name,description,price,image_url) VALUES ('Chicken Burger','Best chicken burger in the city',15,'image');
-INSERT INTO dishes (name,description,price,image_url) VALUES ('Beef Burger','Best beef burger in the city',15,'image');
-INSERT INTO dishes (name,description,price,image_url) VALUES ('Sushi','Best sushi in the city',20,'image');
-INSERT INTO dishes (name,description,price,image_url) VALUES ('Hot Dog','Best hot dog in the city',5,'image');
+INSERT INTO dishes (name,description,price,image_url) VALUES ('Traditional Poutine','Fresh hand-cut french fries,gravy,and Quebec cheese curds',10,'./assests/poutine.jpg');
+
+INSERT INTO dishes (name,description,price,image_url) VALUES ('Veggie Traditional Poutine','Fresh hand-cut french fries,veggie gravy,and Quebec cheese curds',10,'./assests/poutine.jpg');
+
+INSERT INTO dishes (name,description,price,image_url) VALUES ('Butter Chicken Poutine','Fresh hand-cut french fries,gravy,and Quebec cheese curds,grilled chicken, butter chicken sauce,and green onions',13,'./assests/poutine.jpg');
+
+INSERT INTO dishes (name,description,price,image_url) VALUES ('Chicken Inferno Poutine','Fresh hand-cut french fries,gravy,and Quebec cheese curds,grilled chicken, Sriracha,Jalapenos,and red peppers',13,'./assests/poutine.jpg');
+
+INSERT INTO dishes (name,description,price,image_url) VALUES ('Philly Cheesesteak  Poutine','Fresh hand-cut french fries,gravy,and Quebec cheese curds,flat-iron steak,mushrooms,caramelized onions,cheese sauce',14,'./assests/poutine.jpg');
+
+INSERT INTO dishes (name,description,price,image_url) VALUES ('Mac N\'Cheeseburger Poutine','Kraft Dinner macroni,prime ground beef,cheese sauce,sliced pickles,and crushed ketchup chips on a bed of crispy fries',14,'./assests/poutine.jpg');
+
+INSERT INTO dishes (name,description,price,image_url) VALUES ('KD cool and the buffalo Ranch Poutine','Kraft Dinner macroni,fried chicken bites,buffalo sauce,Cool Ranch Doritos,and green onions,on a bed of crispy fries',14,'./assests/poutine.jpg');
+
+INSERT INTO dishes (name,description,price,image_url) VALUES ('BBQ Bacon Mac Poutine','Kraft Dinner macroni,double-smocked bacon,and Forty Creek Barbeque sauce, on a bed of crispy fries',14,'./assests/poutine.jpg');
+
