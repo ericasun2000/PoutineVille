bc8b9ecd224c24167c9215d7b46e09794e4f15e9
Adel Rashed <adel_rashed@outlook.com>
2020-06-27 16:45:56 +0000

    created the seeds for the dishes entity

---
+++ db/seeds/01_dishes.sql
@@ -0,0 +1,4 @@
+INSERT INTO dishes (name,description,price,image_url) VALUES ('Chicken Burger','Best chicken burger in the city',15,'image');
+INSERT INTO dishes (name,description,price,image_url) VALUES ('Beef Burger','Best beef burger in the city',15,'image');
+INSERT INTO dishes (name,description,price,image_url) VALUES ('Sushi','Best sushi in the city',20,'image');
+INSERT INTO dishes (name,description,price,image_url) VALUES ('Hot Dog','Best hot dog in the city',5,'image');
