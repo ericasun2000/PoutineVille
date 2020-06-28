a583be68de58cd1758fb97525fd7b692dd52e61c
Erica Sun <ericasun@seas.upenn.edu>
2020-06-28 15:05:27 +0000

    completed the ajax GET request

--- routes/dishes.js
+++ routes/dishes.js
@@ -5,10 +5,10 @@ module.exports = ({ getDishes, addDish }) => {
   router.get("/", (req, res) => {
     getDishes()
       .then(dishes => {
-        res.json({ dishes });
+        res.json(dishes);
       })
       .catch(err => {
-        res.status(500).json({ error: err.message });
+        res.status(500).json({error: err.message});
       });
   });
 
