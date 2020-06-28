5f9a1556ac5fdbe9df4b6a2b70eda2ae6640a5a4
Erica Sun <ericasun@seas.upenn.edu>
2020-06-28 21:44:05 +0000

    try to send message back to twilio

---
+++ routes/serverSMS.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
+module.exports = () => {
+    router.post('/', (req, res) => {
+        console.log(req.body);
+        const twiml = new MessagingResponse();
+        console.log(twiml);
+
+        twiml.message('The Robots are coming! Head for the hills!');
+
+        res.writeHead(200, {'Content-Type': 'text/xml'});
+        res.end(twiml.toString());
+
+        // res.send(`
+        //   <Response>
+        //     <Message>
+        //       TESSSTT
+        //     </Message>
+        //   </Response>`)
+    })
+    return router;
+
+}
