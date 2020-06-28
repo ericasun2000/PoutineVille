0362e8ac8e319d8cc3fd722ac4999a1b52c88282
Erica Sun <ericasun@seas.upenn.edu>
2020-06-28 16:59:45 +0000

    initial push (not working)

--- helpers/dbHelpers.js
+++ helpers/dbHelpers.js
@@ -1,29 +1,4 @@
 module.exports = db => {
-    const getUsers = () => {
-        const query = {
-            text: `SELECT * FROM users;`,
-        };
-
-        return db.query(query).then(result => result.rows);
-    };
-
-    const addUser = (name, email, password) => {
-        const query = {
-            text: "INSERT INTO users(name) VALUES ($1) RETURNING *",
-            values: [name],
-        };
-
-        return db.query(query).then(result => result.rows);
-    };
-
-    const getWidgets = () => {
-      const query = {
-        text: `SELECT * FROM widgets`,
-      };
-
-      return db.query(query).then(result => result.rows);
-    };
-
     const getDishes = () => {
       const query = {
         text: `SELECT * FROM dishes`,
@@ -32,18 +7,27 @@ module.exports = db => {
       return db.query(query).then(result => result.rows);
     };
 
-    const addDish = (dish_id, order_id, quantity, price) => {
+    const addDish = (wantedDishes, orderId) => {
+      // const queryStr = 'INSERT INTO ordered_dishes(dish_id, order_id, quantity, price) VALUES ';
+      wantedDishes.forEach(wantedDish => {
+        let queryStr = `INSERT INTO ordered_dishes(dish_id, order_id, quantity, price) VALUES (${wantedDish.id}, ${orderId}, ${wantedDish.quantity}, ${wantedDishes.price}) RETURNINIG *`;
+        return db.query(queryStr).then(result => result.rows);
+      })
+      // queryStr += 'RETURNINIG *';
+
+    }
+
+    const addOrder = (phoneNumber) => {
       const query = {
-        text: `INSERT INTO ordered_dishes(dish_id, order_id, quantity, price) VALUES (dish_id, order_id, quantity, price) RETURNING *`,
+        text: `INSERT INTO orders(telephone) VALUES ($1) RETURNING id`,
+        values: [phoneNumber]
       };
-      return db.query(query).then(result => result.rows);
+      return db.query(query).then(result => result.rows[0].id);
     }
 
     return {
-        getUsers,
-        addUser,
-        getWidgets,
         getDishes,
-        addDish
+        addOrder,
+        addDish,
     };
 };
