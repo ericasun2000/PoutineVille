e3c8e41a90003119c881a3c0f78524d15d8ed18b
Erica Sun <ericasun@seas.upenn.edu>
2020-06-28 15:58:24 +0000

    create orderObj

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -21,10 +21,36 @@ const submitOrder = function() {
 }
 
 const getOrderInfo = function() {
+  const inputArray = $('.quantity');
+
+  const wantedDishes = [];
+  for(let input of inputArray) {
+    let dish = $(input);
+    if(dish.val() !== '0') {
+      const wantedDish = {
+        id: parseInt(dish.attr('data-dish-id')),
+        quantity: parseInt(dish.val()),
+        price: parseInt(dish.attr('data-dish-price'))
+      };
+      wantedDishes.push(wantedDish);
+    }
+  }
+  return wantedDishes;
+};
+
+const createOrderObj = function() {
+  const wantedDishes = getOrderInfo();
+  const phoneNumber = $('#phone-number').val();
+
+  return {
+    wantedDishes,
+    phoneNumber
+  };
 
 }
 
 $(document).ready(function() {
   loadDishes();
+
 });
 
