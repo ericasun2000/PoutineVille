81633c8406a574cb1cdd2ce49809d78c40320759
Adel Rashed <adel_rashed@outlook.com>
2020-06-29 22:13:45 +0000

    adds event listener to buttons to calculate total cost

---
+++ public/scripts/calculateTotalCost.js
@@ -0,0 +1,17 @@
+const calculateTotalCost = function() {
+  const total = function () {
+    const InputArray = $(".quantity");
+    let total = 0;
+    for (const input of InputArray) {
+      const price = Number($(input).attr("data-dish-price"));
+      const quantity = Number($(input).val());
+      total += price * quantity;
+    }
+    $("#total-cost").text(`$ ${total}`);
+
+  };
+
+  $(".plus").on("click",total);
+
+$(".minus").on("click",total);
+};
