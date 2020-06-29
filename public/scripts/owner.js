04f2777a410ec7c4f66d6f474f020a5ab2cb2ac0
Adel Rashed <adel_rashed@outlook.com>
2020-06-29 14:41:38 +0000

    listen to order status form

---
+++ public/scripts/owner.js
@@ -0,0 +1,34 @@
+
+$(document).ready(function() {
+
+  $("input[type='radio']").click(function() {
+    if ($("#order-ready").is(":checked")) {
+      $("#order-minutes").prop("disabled",true);
+    } else {
+      $("#order-minutes").prop("disabled",false);
+    }
+
+  });
+
+  $("form").on("submit", function(event) {
+    event.preventDefault();
+    const orderID = Number($("#order-id").val());
+    const message = createMessage();
+
+
+    console.log("orderID",orderID);
+    console.log("message",message);
+    $("input[type='radio']").prop("checked",false);
+
+  });
+
+  const createMessage = function() {
+    if ($("#order-ready").is(":checked")) {
+      return  "Your order is ready";
+    } else {
+      const minutes = $("#order-minutes").val();
+      return `Your order will be ready in ${minutes} minutes`;
+    }
+  };
+});
+
