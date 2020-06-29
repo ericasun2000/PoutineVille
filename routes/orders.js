6185b4c96c80a5fc3730cbe137870ceee01b4ea0
Adel Rashed <adel_rashed@outlook.com>
2020-06-29 19:14:28 +0000

    post order form to /orders instead of /api/dishes

--- routes/orders.js
+++ routes/orders.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const {sendSMS} = require('./send_sms');
+const {sendSMSToOwner} = require('./sendSMSToOwner');
 
 
-module.exports = ({ findNumber }) => {
+module.exports = ({ findNumber,addOrder,addDish }) => {
+
+  router.post("/", (req, res) => {
+    const {wantedDishes, phoneNumber} = req.body;
+    addOrder(phoneNumber)
+      .then(id => addDish(wantedDishes, id))
+      .then(order => {
+        sendSMSToOwner(order);
+        console.log("adding order");
+        res.send({message:"ok"});
+      })
+      .catch(err => console.log(err));
+  });
 
   router.post("/status", (req, res) => {
     const {orderID, message} = req.body;
