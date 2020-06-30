35e79521dc59abb9b5733af22a9420f98c4c4ada
Maribelle Leong <maribelle.leong@gmail.com>
2020-06-30 16:58:20 -0300

    created admin login page

--- routes/admin.js
+++ routes/admin.js
@@ -4,8 +4,14 @@ const path = require('path');
 
 module.exports = () => {
     router.get("/", (req, res) => {
-        res.sendFile(path.join(__dirname + '/../public/owner.html'));
+        //if has cookie 
+        // res.sendFile(path.join(__dirname + '/../public/owner.html'));
+        // if no cookie
+        res.sendFile(path.join(__dirname + '/../public/ownerLogin.html'));
+
     });
+
+
 
     return router;
 };
