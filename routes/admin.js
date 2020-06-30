06941de4dc2bf19296e4c937678e47d03bb926b0
Maribelle Leong <maribelle.leong@gmail.com>
2020-06-30 17:22:01 -0300

    created admin logout route, and customized css for the form

--- routes/admin.js
+++ routes/admin.js
@@ -5,12 +5,28 @@ const path = require('path');
 module.exports = () => {
     router.get("/", (req, res) => {
         //if has cookie 
-        // res.sendFile(path.join(__dirname + '/../public/owner.html'));
-        // if no cookie
-        res.sendFile(path.join(__dirname + '/../public/ownerLogin.html'));
+        if (req.session.isAuthenticated) {
+            res.sendFile(path.join(__dirname + '/../public/owner.html'));
+        } else {
+            res.sendFile(path.join(__dirname + '/../public/ownerLogin.html'));
+        }
 
     });
 
+    router.post("/", (req, res) => {
+        // check inputed password
+        if (req.body.password === "1234") {
+            req.session.isAuthenticated = true;
+            res.sendFile(path.join(__dirname + '/../public/owner.html'));
+        } else {
+            res.sendFile(path.join(__dirname + '/../public/ownerLogin.html'));
+        }
+    });
+
+    router.post("/logout", (req, res) => {
+        req.session = null;
+        res.redirect('/admin');
+    });
 
 
     return router;
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const bodyParser = require("body-parser");
 const sass = require("node-sass-middleware");
 const app = express();
 const morgan = require('morgan');
+const cookieSession = require('cookie-session');
 
 // PG database client/connection setup
 const { Pool } = require('pg');
@@ -33,6 +34,10 @@ app.use("/styles", sass({
   outputStyle: 'expanded'
 }));
 app.use(express.static(__dirname + "/public"));
+app.use(cookieSession({
+  name: 'session',
+  keys: ["hello", "bye"],
+}))
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
