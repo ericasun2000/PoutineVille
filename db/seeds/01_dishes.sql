d18b94ac63cb0c61783010dbec86fc59f34fc245
Erica Sun <ericasun@seas.upenn.edu>
2020-07-01 18:31:43 +0000

    add images to assets and link to database

--- db/seeds/01_dishes.sql
+++ db/seeds/01_dishes.sql
@@ -1,16 +1,16 @@
-INSERT INTO dishes (name,description,price,image_url) VALUES ('Traditional Poutine','Fresh hand-cut french fries, gravy, and Quebec cheese curds',10,'https://az826390.vo.msecnd.net/cdn/media/home/inspiring_recipes/recipes/new_-_p/poutine-general-tao-1160x650-bs009061-pub-68649.ashx');
+INSERT INTO dishes (name,description,price,image_url) VALUES ('Traditional Poutine','Fresh hand-cut french fries, gravy, and Quebec cheese curds',10,'./assets/traditional.jpg');
 
-INSERT INTO dishes (name,description,price,image_url) VALUES ('Veggie Traditional Poutine','Fresh hand-cut french fries, veggie gravy, and Quebec cheese curds',10,'https://d1doqjmisr497k.cloudfront.net/-/media/mccormickcan/recipes/desktop/blt_chicken_poutine_2000.jpg?vd=00010101T000000Z&hash=33844A590EC8EAE4BB80533BA9992773E0DF4717');
+INSERT INTO dishes (name,description,price,image_url) VALUES ('Veggie Traditional Poutine','Fresh hand-cut french fries, veggie gravy, and Quebec cheese curds',10,'./assets/veggie.jpg');
 
-INSERT INTO dishes (name,description,price,image_url) VALUES ('Butter Chicken Poutine','Fresh hand-cut french fries, gravy, and Quebec cheese curds, grilled chicken, butter chicken sauce, and green onions',13,'https://www.sanjeevkapoor.com/UploadFiles/RecipeImages/Butter-Chicken-Poutine.jpg');
+INSERT INTO dishes (name,description,price,image_url) VALUES ('Butter Chicken Poutine','Fresh hand-cut french fries, gravy, and Quebec cheese curds, grilled chicken, butter chicken sauce, and green onions',13,'./assets/butter.jpg');
 
-INSERT INTO dishes (name,description,price,image_url) VALUES ('Chicken Inferno Poutine','Fresh hand-cut french fries, gravy, and Quebec cheese curds, grilled chicken, Sriracha, Jalapenos, and red peppers',13,'https://canardsdulacbrome.com/wp-content/uploads/2018/10/canards-du-lac-brome-recette-poutine-de-canard-R.jpg');
+INSERT INTO dishes (name,description,price,image_url) VALUES ('Chicken Inferno Poutine','Fresh hand-cut french fries, gravy, and Quebec cheese curds, grilled chicken, Sriracha, Jalapenos, and red peppers',13,'./assets/chicken.jpg');
 
-INSERT INTO dishes (name,description,price,image_url) VALUES ('Philly Cheesesteak Poutine','Fresh hand-cut french fries, gravy, and Quebec cheese curds, flat-iron steak, mushrooms, caramelized onions, cheese sauce',14,'https://images.dailyhive.com/20180212095725/Taboo-Cuisine-Rebelle.jpg');
+INSERT INTO dishes (name,description,price,image_url) VALUES ('Philly Cheesesteak Poutine','Fresh hand-cut french fries, gravy, and Quebec cheese curds, flat-iron steak, mushrooms, caramelized onions, cheese sauce',14,'./assets/philly.jpg');
 
-INSERT INTO dishes (name,description,price,image_url) VALUES ('Mac N''Cheeseburger Poutine','Kraft Dinner macroni, prime ground beef, cheese sauce, sliced pickles, and crushed ketchup chips on a bed of crispy fries',14,'https://az826390.vo.msecnd.net/cdn/media/home/inspiring_recipes/recipes/new_-_p/poutine-boeuf-braise-1160x650-brisket-poutine-021-pub-72050.ashx?la=en&mw=1160&w=1160&hash=4A73267DF8E47B8E9E504FAD6D44BFCB2334BEBF');
+INSERT INTO dishes (name,description,price,image_url) VALUES ('Mac N''Cheeseburger Poutine','Kraft Dinner macroni, prime ground beef, cheese sauce, sliced pickles, and crushed ketchup chips on a bed of crispy fries',14,'./assets/mac.jpg');
 
-INSERT INTO dishes (name,description,price,image_url) VALUES ('KD Cool & the Buffalo Ranch Poutine','Kraft Dinner macaroni, fried chicken bites, buffalo sauce, Cool Ranch Doritos, and green onions on a bed of crispy fries',14,'https://149366112.v2.pressablecdn.com/wp-content/uploads/2017/06/buffalo-ranch-jackfruit-poutine-002.jpg');
+INSERT INTO dishes (name,description,price,image_url) VALUES ('KD Cool & the Buffalo Ranch Poutine','Kraft Dinner macaroni, fried chicken bites, buffalo sauce, Cool Ranch Doritos, and green onions on a bed of crispy fries',14,'./assets/kraft.jpg');
 
-INSERT INTO dishes (name,description,price,image_url) VALUES ('BBQ Bacon Mac Poutine','Kraft Dinner macaroni, double-smoked bacon, and Forty Creek Barbeque sauce on a bed of crispy fries',14,'https://lh3.googleusercontent.com/proxy/zEFlF-vhMGlXW9_0omfAXYlr2z0Gu-h4-psHbdkq_ngP8gc8ONxtmEd_m_wPvcZApC6LmHSkUuIm-9eIg5_xKp7Dy-Md_uWICZGm7hoHeQhxRpC3Uzg');
+INSERT INTO dishes (name,description,price,image_url) VALUES ('BBQ Bacon Mac Poutine','Kraft Dinner macaroni, double-smoked bacon, and Forty Creek Barbeque sauce on a bed of crispy fries',14,'./assets/bbq.jpg');
 
