e67d0dd4955aad5c1cb9ee03efcf06f0ab466a08
Maribelle Leong <maribelle.leong@email.com>
2020-07-01 19:33:53 -0300

    completed creating chart on canvas

--- public/scripts/analytics.js
+++ public/scripts/analytics.js
@@ -1,27 +1,62 @@
-var ctx = document.getElementById('myChart').getContext('2d');
-var chart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'pie',
+const generateChart = (dishArr) => {
+    const resultObj = generateArrays(dishArr);
 
-    // The data for our dataset
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
-        }]
-    },
+    const backgroundColors = ['rgb(250,213,92)', 'rgb(38,125,179)', 'rgb(133,97,200)', 'rgb(237,102,71)', 'rgb(109,219,219)', 'rgb(104,193,130)', 'rgb(150, 139, 195)', 'rgb(227,113,178)']
 
-    // Configuration options go here
-    options: {}
-});
+    const ctx = document.getElementById('myChart').getContext('2d');
+    const chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'bar',
+
+        // The data for our dataset
+        data: {
+            labels: resultObj.labelArr,
+            datasets: [{
+                backgroundColor: backgroundColors,
+                // borderColor: 'rgb(255, 99, 132)',
+                data: resultObj.sumArr
+            }]
+        },
+
+        // Configuration options go here
+        options: {
+            legend: {
+                display: false
+            },
+            title: {
+                display: true,
+                text: 'Sales of PoutineVille'
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+
+}
+
+const generateArrays = (dishArr) => {
+    const labelArr = [];
+    const sumArr = [];
+
+    for (let dish of dishArr) {
+        labelArr.push(dish.name);
+        sumArr.push(Number(dish.sum));
+    }
+
+    return {
+        labelArr,
+        sumArr
+    }
+}
 
 $(document).ready(function () {
-    $.get('/admin/analysisData', function (data) {
-        console.log('inside ajax');
-        console.log(data);
+    $.get('/admin/analysisData', function (dishArr) {
+        generateChart(dishArr);
     })
         .done(() => console.log('Done with AJAX GET request'))
         .fail(() => console.log('Oops! Problem with GET  request'));
