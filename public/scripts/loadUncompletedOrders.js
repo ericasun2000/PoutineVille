948e78e9b59ea0496a9e9e7c9cce7d25152c4196
Maribelle Leong <maribelle.leong@email.com>
2020-07-02 23:09:37 -0300

    updated the uncompleted order every 5s

--- public/scripts/loadUncompletedOrders.js
+++ public/scripts/loadUncompletedOrders.js
@@ -1,25 +1,24 @@
 const loadUncompletedOrders = () => {
-  const time = 0.5 * 60 * 1000; // minutes * 60s/min * 1000ms/s
-
+  const time = 5 * 1000; // minutes * 60s/min * 1000ms/s
   $.ajax({
     method: "GET",
     url: "/orders/uncompleted",
     dataType: "json",
-    success: function(orders) {
+    success: function (orders) {
       $("#uncompleted-orders").empty();
       renderOrders(orders);
 
     },
-
-    complete:() => setTimeout(loadUncompletedOrders,time)
+    complete: () => setTimeout(loadUncompletedOrders, time)
   });
 };
-const renderOrders = function(orders) {
+
+const renderOrders = function (orders) {
   for (const order of orders) {
     $("#uncompleted-orders").append(createOrdersElement(order));
   }
 };
 
-const createOrdersElement = function(order) {
+const createOrdersElement = function (order) {
   return `<li> Order ID : ${order.id}</li>`;
 };
--- public/scripts/owner.js
+++ public/scripts/owner.js
@@ -1,53 +1,54 @@
 /* eslint-disable no-undef */
 
-$(document).ready(function() {
+$(document).ready(function () {
   // postion notification top center of page
   toastr.options.positionClass = "toast-top-center";
   loadUncompletedOrders();
 
-  $("input[type='radio']").click(function() {
+  $("input[type='radio']").click(function () {
     if ($("#order-ready").is(":checked")) {
-      $("#order-minutes").prop("disabled",true);
+      $("#order-minutes").prop("disabled", true);
     } else {
-      $("#order-minutes").prop("disabled",false);
+      $("#order-minutes").prop("disabled", false);
     }
 
   });
 
-  $(".owner-form").on("submit", function(event) {
+  $(".owner-form").on("submit", function (event) {
     console.log('inside ajax post order---- hello');
     event.preventDefault();
     const orderID = Number($("#order-id").val());
     const message = createMessage();
     const status = $("#order-ready").is(":checked");
-    console.log("status",status);
+    console.log("status", status);
 
     $.ajax({
       url: "/orders/status",
       method: "POST",
-      data: JSON.stringify({orderID,message,status}),
+      data: JSON.stringify({ orderID, message, status }),
       contentType: "application/json; charset=utf-8",
       dataType: "json"
     }).then(() => {
       toastr.success('Success');
       //clear all input after submission
-      $("input[type='radio']").prop("checked",false);
+      $("input[type='radio']").prop("checked", false);
       $("#order-id").val("");
       $("#order-minutes").val("");
+      loadUncompletedOrders();
 
     })
       .catch(() => toastr.error("An error occurred. Please submit again"));
 
 
-    console.log("orderID",orderID);
-    console.log("message",message);
+    console.log("orderID", orderID);
+    console.log("message", message);
 
 
   });
 
-  const createMessage = function() {
+  const createMessage = function () {
     if ($("#order-ready").is(":checked")) {
-      return  "Your order is ready";
+      return "Your order is ready";
     } else {
       const minutes = $("#order-minutes").val();
       return `Your order will be ready in ${minutes} minutes`;
